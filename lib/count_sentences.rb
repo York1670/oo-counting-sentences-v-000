ef6adafc94ef59276069eb900d3ef59b65cdde1f
Bobbi York <york1670@gmail.com>
2018-11-04 20:04:59 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     try = self.split(/[.?!]/).count
-    final = self.select {|try| try.include? try > 0}
+    final = try.select {|try| try.include? try > 0}
   end
 end
